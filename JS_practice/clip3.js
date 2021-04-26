f7d4fd3bf0828f713debfe893bcd392cb7e70117
Peniel Cho <pensurf@naver.com>
2021-04-26 23:07:04 +0900

    data types

--- JS_practice/clip3.js
+++ JS_practice/clip3.js
@@ -1,1 +1,99 @@
 console.log('Hello Javascript')
+
+{
+    const name = 'Mark';
+    console.log(name);
+}
+
+let age = 37;
+
+console.log(age);
+
+{
+    age++;
+    console.log(age);
+}
+
+var a = 0;
+
+(function () {
+        a++;
+        console.log(a);
+    })();
+
+    console.log(a);
+
+{ var adoy = 37;}
+console.log(adoy);
+
+
+    var c = 0;
+{
+    c++;
+    console.log(c);
+}
+
+{ var d =0;
+console.log(d);}
+
+console.log(d);
+
+
+    console.log(name);
+
+    name='Mark';
+
+    console.log(name);
+
+    var name='Renjun';
+
+    let whatever = 'Mark';
+    whatever = 37;
+    whatever = true;
+    // 값에 따라서 변수가 가진 자료형이 달라지는데 이를 동적타이핑이라고 함. dynamic type language
+
+const isTrue = true;
+const isFalse = false;
+
+console.log(isTrue, typeof isTrue);
+console.log(isFalse, typeof isFalse);
+
+const allan = null;
+console.log(allan, typeof allan);
+
+
+
+
+
+
+let b;
+console.log(b, typeof b);
+
+b=undefined;
+console.log(b, typeof b);
+
+
+
+if (allan==b) {
+    console.log(allan==b);
+}
+
+// true가 나옴. 즉 null과 undefined는 == 비교시 같은 것으로 여겨짐.
+// 때문에 ===으로 비교해야함. ===은 값뿐만 아니라 type까지 나오기 때문에 정확하게 비교가 가능함.
+
+if (allan===b) {
+    console.log(allan===b);
+}
+
+const r = Number('Mark');
+
+console.log(r, typeof r);
+
+
+const dr = 'Mark';
+const eam = `${dr} Lee`;
+
+console.log(eam);
+
+
+const a Symbol();
